37d56dbcc73dcb331081972836846800ebe02678
Brian Futrell <bfutrel@gmail.com>
2018-09-12 07:58:23 -0400

    finished lesson 3 video 2 (lesson 9) - didn't change filename
    finished lesson 3 video 2 (lesson 9) - didn't change filename

--- lesson8.js
+++ lesson8.js
@@ -1,3 +1,13 @@
 $(function() {
+    $("#img1").attr({ src: function() {
+        return "https://via.placeholder.com/450x350?text=This works!";
+    } });
+    
+    //$('img').attr(
+    //{src: "https://via.placeholder.com/150x250/eee/ff00ff",
+    //alt:"Image name", heigt: '150px', width: '150px'}
+    //);
+    
+    $('.highlight').attr("style", "color: red");
 })
 
