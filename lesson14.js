65afcffd47e748e7f757d9956b5b7cfe2bcf201b
Brian Futrell <bfutrel@gmail.com>
2018-09-20 05:27:43 -0400

    finished section 3.7 (lesson 14)

--- lesson14.js
+++ lesson14.js
@@ -1,4 +1,26 @@
 $(function () {
     // section 3, video 7
+    $('li, p').click(function () {
+        // jQuery 'this'
+        // shows the DOM for the element
+        console.log($(this));
 
+        // JavaScript 'this'
+        // merely shows the HTML element
+        console.log(this);
+    });
+
+    // alternative method to set up a click method.
+    // NOTE: this method has been depracated.
+//    $('p').bind('click', function() {
+//       console.log("Bind clicked");
+//    });
+
+    $('img').on('click', function() {
+        console.log("Image clicked");
+    });
+
+    $('a').on('click', function(e) {
+        e.preventDefault();
+    })
 });
