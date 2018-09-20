24e9475ebe6fc617dcfbdfc51311686d73a7fddb
Brian Futrell <bfutrel@gmail.com>
2018-09-20 12:11:50 -0400

    finished section 1.8 (lesson 16)

--- lesson16.js
+++ lesson16.js
@@ -1,4 +1,17 @@
 $(function () {
     // section 3, video 9
 
+    $('.btn').click(function() {
+
+        // map generates an array with the contents returned from its function.
+        var eleHighlight = $('.highlight').map(function() {
+            return $(this).html();
+        });
+
+        console.log(eleHighlight[2]);
+
+        // starts with the list item <li> at index 3 to index 7 and sets the background color to yellow.
+        $('li').slice(3,7).css("background-color", "yellow");
+
+    });
 });
