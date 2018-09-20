4de36d16b1a58efb9b6391f4ab6c23770739d72f
Brian Futrell <bfutrel@gmail.com>
2018-09-20 11:14:49 -0400

    finished section 3.8 (lesson 15), added launch profile for VS Code

--- lesson15.js
+++ lesson15.js
@@ -1,5 +1,26 @@
 $(function () {
     // section 3, video 8
 
+    // $('li').click(function() {
 
+    //     // filters the list items to only those with the class of 'highlight'.
+    //     //$('li').filter('.highlight').css('color', 'red');
+
+    //     // changes the clicked list item to red if it has a class of 'highlight'.
+    //     $(this).filter('.highlight').css('color', 'red');
+    // })
+
+    // // when a paragraph is clicked set the first list item text to blue.
+    // $('p').click(function() {
+    //     console.log($('li'));
+
+    //     $('li').first().css('color', 'blue');
+    // });
+
+    // when a paragraph is clicked set the text for list items that contain a 'span' to green.
+    $('p').click(function() {
+        console.log($('li'));
+
+        $('li').has('span').css('background-color', 'green');
+    });
 });
