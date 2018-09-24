72bcabade57c86c7bd3e0dbffa6ff284dddc6fab
Brian Futrell <bfutrel@gmail.com>
2018-09-24 06:11:14 -0400

    finished section 3.10

--- lesson17.js
+++ lesson17.js
@@ -1,5 +1,24 @@
 $(function () {
     // section 3, video 10
 
+    $('ul').attr('title', 'List title');
+
+    // lists all child elements listed in a paragraph element
+    $('.btn').click(function() {
+        var eleKids = $('p').children();
+
+        console.log(eleKids);
+    });
+
+    // when a span is clicked get the span's parent element and
+    // log it to the console.
+    $('span').click(function() {
+        var eleDad = $(this).parent().parent();
+
+        console.log(eleDad);
+
+        console.log(eleDad.attr('title'));
+    });
+
 
 });
