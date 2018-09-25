17cbb119ebd9f3c20b7ffc6d2527fe0d6c6ad848
Brian Futrell <bfutrel@gmail.com>
2018-09-24 20:38:22 -0400

    finished Section 3.12 (lesson 19)

--- lesson19.js
+++ lesson19.js
@@ -1,50 +1,18 @@
 $(function () {
-    // section 3, video 11
-
-    $('ul').attr('title', 'List title');
-
-    // when a span is clicked get the closest 'ul' element and
-    // log it to the console.
-    // parent, grandparent, etc.
-    /*$('span').click(function() {
-        var eleDad = $(this).closest('ul');
-
-        console.log(eleDad);
-
-        console.log(eleDad.attr('title'));
-    });*/
-
-    // when a span sleement is clicked looks for the next span
-    // and sets the text color to red.
-    $('span').click(function() {
-        $(this).next().css('color', 'red');
-
-        // finds the previous span element and sets the text color
-        // to green.
-        $(this).prev().css('color', 'green');
-
+    // section 3, video 12
+    $('span').css({
+        "border-color": "red",
+        "border-width": "1px",
+        "border-style": "solid",
+        "padding": "1px"
     });
 
-    $('li').click(function() {
-        /*
-        // finds the previous list element and sets the text color
-        // to red.
-        $(this).next().css('color', 'red');
 
-        // finds the previous list element and sets the text color
-        // to green.
-        $(this).prev().css('color', 'green');
+    $('span').click(function () {
+        $(this).siblings().css('color', 'blue');
 
-        // sets the text of all list items after the current one
-        $(this).nextAll().text("I'm next!");
-
-        // sets the text of all previous list items before the current one.
-        $(this).prevAll().text("I'm previous!");
-        */
-
-        // finds all spans within the currently selected list item and sets
-        // the color to yellow
-        $(this).find('span').css('color', 'yellow');
-    })
-
+        // selects all elements at the same level and sets the text
+        // same parent, but would have different children.
+        $(this).siblings().text("I'm a brother or a sister!");
+    });
 });
