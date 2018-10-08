4baa89b3fdaceed1915448a277d03989f15bd625
Brian Futrell <bfutrel@gmail.com>
2018-10-08 08:10:43 -0400

    finished section 8.2

---
+++ Section_8/lesson21.js
@@ -0,0 +1,29 @@
+$(function () {
+    $('.btn').click(function() {
+
+        // get only checkboxes that are checked
+        // with '.val()' it only returns the first checked item
+        // var checkedButtons = $('input:checked').val();
+        // console.log(checkedButtons);
+
+        var firstName = $('input[name="first"]').val();
+        var lastName = $('input[name="last"]').val();
+        $('h1').html(firstName + " " + lastName);
+
+        // this only picks up on the initial value of the input fields
+        // since that is what is assigned to the DOM.
+        $('input[value="jquery"]').val("YES it's jQuery");
+    })
+
+    $('form').submit(function(e) {
+        e.preventDefault();
+        console.log("Please don't submit me!");
+
+        if ($('input:checked').length > 0) {
+            $('form').css('background-color', 'blue');
+        }
+        else {
+            $('form').css('background-color', 'red');
+        }
+    })
+})
