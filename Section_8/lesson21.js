984ad5b533324d1cfa219f8d64292b3fbb927a8e
Brian Futrell <bfutrel@gmail.com>
2018-10-09 08:16:23 -0400

    finished section 8.3

--- Section_8/lesson21.js
+++ Section_8/lesson21.js
@@ -3,16 +3,20 @@ $(function () {
 
         // get only checkboxes that are checked
         // with '.val()' it only returns the first checked item
-        // var checkedButtons = $('input:checked').val();
-        // console.log(checkedButtons);
+        // 10/9/18 - using the map function allows the current values of the
+        //           checkboxes to be used.
+        var checkedButtons = $('input:checked').map(function() {
+            return $(this).val();
+        });
+        console.log(checkedButtons);
 
-        var firstName = $('input[name="first"]').val();
-        var lastName = $('input[name="last"]').val();
-        $('h1').html(firstName + " " + lastName);
+        // var firstName = $('input[name="first"]').val();
+        // var lastName = $('input[name="last"]').val();
+        // $('h1').html(firstName + " " + lastName);
 
         // this only picks up on the initial value of the input fields
         // since that is what is assigned to the DOM.
-        $('input[value="jquery"]').val("YES it's jQuery");
+        //$('input[value="jquery"]').val("YES it's jQuery");
     })
 
     $('form').submit(function(e) {
