bf23dc0d6cdc1406a755df7167b668d337b0d716
Brian Futrell <bfutrel@gmail.com>
2018-10-11 14:21:28 -0400

    working through section 9.2  (at 3:32)

--- Section_9/lesson23.js
+++ Section_9/lesson23.js
@@ -1,3 +1,16 @@
 $(function() {
     // Section 9.2
+    $('input').focus((function() {
+        $(this).css('background-color', 'yellow');
+    }))
+
+    $('input').blur((function() {
+        $(this).css('background-color', 'white');
+    }))
+
+    $('input').keypress(function(e) {
+        console.log(e.key);
+    })
+
+
 })
