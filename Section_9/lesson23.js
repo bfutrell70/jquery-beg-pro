1bff3d1c0ca1f618e64eed63934f039e30227beb
Brian Futrell <bfutrel@gmail.com>
2018-10-11 14:22:47 -0400

    comments added to describe the event handlers

--- Section_9/lesson23.js
+++ Section_9/lesson23.js
@@ -1,13 +1,17 @@
 $(function() {
     // Section 9.2
+
+    // input field receives focus
     $('input').focus((function() {
         $(this).css('background-color', 'yellow');
     }))
 
+    // input field loses focus
     $('input').blur((function() {
         $(this).css('background-color', 'white');
     }))
 
+    // input field receives a keypress
     $('input').keypress(function(e) {
         console.log(e.key);
     })
