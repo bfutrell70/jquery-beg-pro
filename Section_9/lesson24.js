76184d2fc88bf73a13fc0fa40a193f3bd48e9757
Brian Futrell <bfutrel@gmail.com>
2018-10-11 22:01:52 -0400

    finished section 9.2

--- Section_9/lesson24.js
+++ Section_9/lesson24.js
@@ -1,3 +1,13 @@
 $(function() {
-    
+    // section 9.3 - browser events
+
+    // handles resizing the browser window
+    $(window).resize(function() {
+        $('.highlight').html($(window).width());
+        //console.log($(window).width());
+    });
+
+    $(window).scroll(function() {
+        console.log('scrolling');
+    });
 })
