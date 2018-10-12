3f146b28b0c75f9bd271f44c1427584cecf5585a
Brian Futrell <bfutrel@gmail.com>
2018-10-11 21:48:41 -0400

    finished section 9.2

--- Section_9/lesson23.js
+++ Section_9/lesson23.js
@@ -1,5 +1,13 @@
 $(function() {
-    // Section 9.2
+    // Section 9.2 - keyboard events
+
+    // handles a user choosing an option from the 'select' element
+    $('.sel').change(function() {
+        //$('.highlight').css('color', $(this).val());
+        $('body').css('background-color', $(this).val());
+    })
+
+    var newText = '';
 
     // input field receives focus
     $('input').focus((function() {
@@ -7,14 +15,28 @@ $(function() {
     }))
 
     // input field loses focus
-    $('input').blur((function() {
+    // $('input').blur((function() {
+    //     $(this).css('background-color', 'white');
+    // }))
+    // similar to blur
+    $('input').focusout((function() {
         $(this).css('background-color', 'white');
     }))
 
     // input field receives a keypress
     $('input').keypress(function(e) {
         console.log(e.key);
+        newText += e.key;
+        $('.highlight').text(newText);
     })
 
+    // triggers while a key is pressed
+    $('input').keydown(function(e) {
+        $(this).css('color', 'red');
+    })
 
+    // triggers when a key is released
+    $('input').keyup(function(e) {
+        $(this).css('color', 'green');
+    })
 })
