f2c0fa130b839c5c41f0feaa022ed99e366edb05
Brian Futrell <bfutrel@gmail.com>
2018-10-12 07:42:36 -0400

    finished section 10.1

--- Section_10/lesson26.js
+++ Section_10/lesson26.js
@@ -1,3 +1,25 @@
 $(function() {
+    // Section 10.1
+    $('.highlight').css({
+        backgroundColor: 'red',
+        padding: '10px',
+        margin: '5px',
+        border: '1px solid #ddd'
+    });
+
+    $('.highlight').click(function(){
+        $(this).hide(1000, function() {
+            // is called when the hide is complete
+            $(this).next().css('background-color', 'blue');
+        });
+        // toggles between hide and show
+        $('h1').toggle();
+    });
+
     
+    $('.sel').change(function() {
+        $('.highlight').show(500, function () {
+            $('.highlight').css('background-color', $('.sel').val());
+        })
+    });
 })
