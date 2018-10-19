bfe3a1d502440e40fa7822654f19d12d61cc9262
Brian Futrell <bfutrel@gmail.com>
2018-10-19 14:04:36 -0400

    finished section 10.7

--- Section_10/lesson32.js
+++ Section_10/lesson32.js
@@ -1,11 +1,16 @@
 $(function() {
     // Section 10.7!
+    // jQuery forms and events
 
     // NOTE: stopped at 2:10 in the video.
 
     // making sure the checkbox is checked
 
     $('button').on('click', submitForm);
+
+    $('input[name="agree2"]').change(function() {
+        alert('Checked button ' + $(this).attr('name'));
+    })
 
     function submitForm() {
         // serializes form elements into a string
@@ -14,16 +19,40 @@ $(function() {
         // splits the form values into separate elements
         var tempHolder = formValues.split('&');
 
+        var $agree1 = $('input[name="agree1"]');
+        var $agree2 = $('input[name="agree2"]');
+
+        if ($agree1.prop('checked'))
+        {
+            outputUpdate(tempHolder);
+        }
+        else
+        {
+            alert('You must agree to the TOS');
+        }
+    };
+
+    function outputUpdate(tempHolder) {
         $('#mainDiv').empty();
+        $('input').css('background-color', 'white');
+
         $.each(tempHolder, function(index, value) {
             var itemParts = value.split('=');
 
             var el = $('input[name="' + itemParts[0] + '"]');
-            // conditionally applies a red or white background to the input element 
-            // based on if it has a value or not
-            el.css('background-color', el.val() == "" ? 'red' : 'white');
+            
+            el.nextAll('.error').remove();
+
+            if (el.val() == '') {
+                el.css('background-color', 'red');
+                //el.nextAll('.error').remove();
+                el.after('<div class="error">' + itemParts[0] +' is missing</div>');    
+            }
+            else {
+                //el.nextAll('.error').remove();
+            }
 
             $('#mainDiv').append(itemParts[0] + '\'s value is ' + itemParts[1] + '<br>');
         })
-    };
+    }
 })
