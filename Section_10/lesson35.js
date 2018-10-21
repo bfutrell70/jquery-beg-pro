08264a2406e8b6761cfd62297b3e508b58b10727
Brian Futrell <bfutrel@gmail.com>
2018-10-20 20:35:29 -0400

    finished section 10.10

--- Section_10/lesson35.js
+++ Section_10/lesson35.js
@@ -1,4 +1,12 @@
 $(function() {
     // section 10.10
     // page manipulation classes
+
+    $('#topnav ul li a').click(function() {
+        // remove 'active' class from all links within the nav div
+        $('#topnav ul li a').removeClass('active');
+
+        // add the 'active' class to the currently selected anchor element.
+        $(this).addClass('active');
+    })
 })
