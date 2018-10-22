8884cdfbc4a13f324b0c02fc691246e0c14e3c8b
Brian Futrell <bfutrel@gmail.com>
2018-10-22 16:02:59 -0400

    partially through section 10.11

--- Section_10/lesson36.js
+++ Section_10/lesson36.js
@@ -1,5 +1,13 @@
 $(function() {
     // section 10.11
 
-    
+    // form error classes
+
+    $('button').on('click', submitForm);
+
+    function submitForm() {
+        var $el = $('imput');
+
+        console.log($el);
+    }
 })
