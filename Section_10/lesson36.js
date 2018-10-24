605a340486b266575582d551daff1b8b23a0e65e
Brian Futrell <bfutrel@gmail.com>
2018-10-24 08:08:57 -0400

    finished section 10.11

--- Section_10/lesson36.js
+++ Section_10/lesson36.js
@@ -6,8 +6,20 @@ $(function() {
     $('button').on('click', submitForm);
 
     function submitForm() {
-        var $el = $('imput');
-
+        var $el = $('input[type="text"], input[type="password"]');
+        $el.prev('label').removeClass('error');
         console.log($el);
+
+        for (x=0; x < $el.length; x++)
+        {
+            // this statement won't work since $el[x] is based on the DOM.
+            //console.log($el[x].val());
+            console.log($el.eq(x).val());
+
+            console.log($el.eq(x).attr("name"));
+            if($el.eq(x).val() == '') {
+                $el.eq(x).prev('label').addClass('error');
+            }
+        }
     }
 })
