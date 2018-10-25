b11e993c3f6cfdac0a0b2c5ec5489125e3ff76c2
Brian Futrell <bfutrel@gmail.com>
2018-10-25 07:44:52 -0400

    finished section 10.12

--- Section_10/lesson37.js
+++ Section_10/lesson37.js
@@ -1,4 +1,15 @@
 $(function() {
     // section 10.12
+    $('h1').on('click', function() {
+        var docHeight = $(document).width();
+        var winHeight = $(window).width();
+        console.log($(document));
+        console.log($(window));
+        console.log(docHeight);
+        console.log(winHeight);
 
+        var $el = $('#wrapper > div').last();
+        console.log($el);
+        console.log($el.height(), $el.width());
+    })
 })
