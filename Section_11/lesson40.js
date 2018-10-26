86573810716325c83426db5c577f16f975306ec3
Brian Futrell <bfutrel@gmail.com>
2018-10-26 07:58:41 -0400

    added launch profile for section 11, finished section 11.1

--- Section_11/lesson40.js
+++ Section_11/lesson40.js
@@ -2,5 +2,26 @@ $(function() {
     // section 11.1
     // jQuery slide animation
 
-    
+    $('.btn')
+        .css({
+            border: '1px solid black', 
+            padding: '10px',
+            width: '150px'}
+        )
+        .click(function() {
+            console.log($(this).index());
+
+            if ($(this).index() == 1)
+            {
+                $('#output2').slideUp(5000);
+            }
+            if ($(this).index() == 2)
+            {
+                $('#output2').slideDown(1000);
+            }
+            if ($(this).index() == 3)
+            {
+                $('#output2').slideToggle("fast");
+            }
+        })  
 })
