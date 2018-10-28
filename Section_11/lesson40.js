e09b94fb967c1cefb671b73fae23bded84479c55
Brian Futrell <bfutrel@gmail.com>
2018-10-28 15:00:48 -0400

    finished section 11.1

--- Section_11/lesson40.js
+++ Section_11/lesson40.js
@@ -6,21 +6,25 @@ $(function() {
         .css({
             border: '1px solid black', 
             padding: '10px',
-            width: '150px'}
-        )
+            width: '150px'
+        })
         .click(function() {
+            // index of the element that contains the 'btn' class that was clicked
             console.log($(this).index());
 
             if ($(this).index() == 1)
             {
+                // moves the div up and hides it
                 $('#output2').slideUp(5000);
             }
             if ($(this).index() == 2)
             {
+                // moves the div down and shows it
                 $('#output2').slideDown(1000);
             }
             if ($(this).index() == 3)
             {
+                // toggles between showing and hiding the div
                 $('#output2').slideToggle("fast");
             }
         })  
