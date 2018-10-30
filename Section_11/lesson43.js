b4529f5326f5f4f29f2fd9ac13b2deeb4722ecaa
Brian Futrell <bfutrel@gmail.com>
2018-10-29 22:29:36 -0400

    finished section 11.4

--- Section_11/lesson43.js
+++ Section_11/lesson43.js
@@ -1,4 +1,39 @@
 $(function() {
     // section 11.4
+    // jQuery Animate Even More
+    // stopped at 1:17
+
+    // in order for the 'left' animate step to work the position has to be set
+    // to relative or absolute
+    $('#output1').css({position: 'relative'})
+    //$('#output1').css({position: 'absolute'})
+
+    $('.btn').css({
+        border: '1px solid black',
+        width: '150px',
+        padding: '10px'
+    });
+
+    $('.btn').first().click(function() {
+        $('#output1').animate({
+            'left': '+=50px'
+        }, 'slow');
+    });
+
+    $('.btn').eq(1).click(function() {
+        $('#output1').animate({
+            'left': '-=50px',
+            opacity: 'toggle'
+        }, 'slow', 
+        function() {
+            $('#output2').css('background-color', 'red');
+        });
+    });
+
+    $('.btn').last().click(function() {
+        $('#output2').animate({
+            height: 'toggle'
+        }, 'slow');
+    });
 
 })
