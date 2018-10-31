995a1d321786eea34dad939ee27e62f3090d4650
Brian Futrell <bfutrel@gmail.com>
2018-10-31 12:12:56 -0400

    finished section 11.7

--- Section_11/lesson46.js
+++ Section_11/lesson46.js
@@ -1,9 +1,40 @@
 $(function() {
     // section 11.7
-    // jQuery Animation Fun Plus UI
+    // jQuery Utilities Explored
 
-    $('.btn').click(function() {
+    var myArray = ['hello', 'world', 55];
 
+    $('#output1, #output2').css({
+        'position': 'relative'
     });
+
+    $('.btn').css({
+        'border': '1px solid black',
+        'width': '150px',
+        'padding': '10px'
+    });
+
+    $('.btn').first().click(function() {
+        // checks if a variable is an array
+        if ($.isArray(myArray))
+        {
+            // loops through elements in the array
+            $.each(myArray, function(index, value) {
+                console.log(index, value)
+            })
+        }
+    });
+
+    $('.btn:eq(1)').click(function() {
+        var tempVal = $.trim($('#input1').val());
+
+        // checks to see if the value in tempVal exists in myArray
+        // -1 = not in array, other value = index of the array that matches the value
+        console.log($.inArray(tempVal, myArray));
+    })
+
+    $('.btn').last().click(function() {
+
+    })
 
 })
