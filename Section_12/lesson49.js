c29b25ab8646e41e8e8a55184eb5e006134abd5c
Brian Futrell <bfutrel@gmail.com>
2018-11-01 13:14:18 -0400

    before starting section 12.4

---
+++ Section_12/lesson49.js
@@ -0,0 +1,40 @@
+$(function() {
+    // section 12.4
+    // jquery
+
+    $('#output1, #output2').css({
+        'position': 'relative'
+    });
+
+    $('.btn').css({
+        'border': '1px solid black',
+        'width': '150px',
+        'padding': '10px'
+    });
+
+    $('.btn').click(function() {
+        if ($(this).hasClass('btn-1')) {
+            // must view the page using http/https instead of file
+            // I added the Live Server extesion in VS Code to do this
+            // https://stackoverflow.com/questions/30039512/how-to-view-my-html-code-in-browser-with-visual-studio-code/48721885#48721885
+            $('h1').load('hello.txt')
+        }
+
+        if ($(this).hasClass('btn-2')) {
+            // must view the page using http/https instead of file
+            // I added the Live Server extesion in VS Code to do this
+            // https://stackoverflow.com/questions/30039512/how-to-view-my-html-code-in-browser-with-visual-studio-code/48721885#48721885
+            
+            // load entire contents into h1 tag
+            $('h1').load('hello.html')
+
+            // only loads the element with a class of one from the HTML file
+            //$('h1').load('hello.html .one')
+            
+            // only loads the element with an ID of two from the HTML file
+            //$('h1').load('hello.html #two')
+        }
+
+    });
+    
+})
