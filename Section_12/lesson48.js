66fa39e7b1a9864fcc7a7d3c580a72aad1cb71e6
Brian Futrell <bfutrel@gmail.com>
2018-11-01 12:54:04 -0400

    before starting section 12.3

---
+++ Section_12/lesson48.js
@@ -0,0 +1,25 @@
+$(function() {
+    // section 12.3
+    // jquery AJAX Load Data
+
+    $('#output1, #output2').css({
+        'position': 'relative'
+    });
+
+    $('.btn').css({
+        'border': '1px solid black',
+        'width': '150px',
+        'padding': '10px'
+    });
+
+    $('.btn').click(function() {
+        if ($(this).hasClass('btn-1')) {
+            // must view the page using http/https instead of file
+            // I added the Live Server extesion in VS Code to do this
+            // https://stackoverflow.com/questions/30039512/how-to-view-my-html-code-in-browser-with-visual-studio-code/48721885#48721885
+            $('h1').load('hello.txt')
+        }
+    });
+
+    
+})
