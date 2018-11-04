edc4fa41daf2d617475594e7d277e27e0e009941
Brian Futrell <bfutrel@gmail.com>
2018-11-03 20:47:05 -0400

    finished section 12.8

---
+++ Section_12/lesson53.js
@@ -0,0 +1,33 @@
+$(function() {
+    // section 12.8
+    // Web APIs AJAX
+
+    $('#output1, #output2').css({
+        'position': 'relative'
+    });
+
+    $('.btn').css({
+        'border': '1px solid black',
+        'width': '150px',
+        'padding': '10px'
+    });
+
+    $('.btn-1').click(function() {
+        $.ajax({
+            url: 'https://api.randomuser.me/',
+            // optional - jQuery attempts to determine the MIME type of data from the URL
+            dataType: 'json',
+            success:(function(data) {
+                console.log(data);
+                var randomPic = data.results[0].picture.large;
+                var randomName = data.results[0].name.first + " "  + data.results[0].name.last;
+
+                var html = '<img src="' + randomPic + '">' +
+                            '<p>' + randomName + '</p>'
+
+                $('#output1').html(html);
+            })
+        })
+    })
+    
+})
