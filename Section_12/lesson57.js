8f93d2b1a52112b4365383779e34c331af8eb7c7
Brian Futrell <bfutrel@gmail.com>
2018-11-05 13:17:10 -0500

    finsihed section 12.12

--- Section_12/lesson57.js
+++ Section_12/lesson57.js
@@ -1,6 +1,6 @@
 $(function() {
-    // section 12.11
-    // AJAX jQuery Post Data
+    // section 12.12
+    // jQuery ajax post
 
     $('#output1, #output2').css({
         'position': 'relative'
@@ -32,6 +32,34 @@ $(function() {
               data.sel  
             );
         })
+    })
+
+    $('.btn-2').click(function() {
+        // This URL may or may not work in the future....
+        // as of 11/5/2018 it does.
+        // it simply accepts JSON data an returns the same data in JSON.
+        var urlToPost = "http://s179017901.onlinehome.us/discoveryvip/";
+
+        // serializes names and data for submission
+        var myData = $('#myForm').serialize();
+        console.log(myData);
+
+        $.ajax({
+            type: 'POST',
+            url: urlToPost,
+            data: myData,
+            success: (function(data) {
+                console.log(data);
+
+                $('#output1').append(
+                    "thank you for your submission:<br>" +
+                    data.input1 + "<br>" +
+                    data.input2 + "<br>" + 
+                    data.sel  
+                );
+            })
+        })
+
     })
 
 })
