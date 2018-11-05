b9c30b61024d76c02e12ccf97dc180b90a494829
Brian Futrell <bfutrel@gmail.com>
2018-11-05 14:26:03 -0500

    coded btn-1, working on btn-2

--- Section_12/lesson58.js
+++ Section_12/lesson58.js
@@ -12,12 +12,22 @@ $(function() {
         'padding': '10px'
     });
 
+    // jokes
     $('.btn-1').click(function() {
+        var url = 'http://api.icndb.com/jokes/random/';
+        $.getJSON(url, function(data) {
+            $('#output1').html(data.value.joke);
+        });
 
     })
 
+    // appears to be a paged list of Star Wars characters
     $('.btn-2').click(function() {
-
+        var url = 'https://swapi.co/api/people/?format=json';
+        $.getJSON(url, function(data) {
+            console.log(data)
+            //$('#output1').html(data);
+        });
     })
 
     $('.btn-3').click(function() {
