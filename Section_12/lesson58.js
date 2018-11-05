defbaa9105c2816866e92d915a73299ff5cfb1bf
Brian Futrell <bfutrel@gmail.com>
2018-11-05 13:41:39 -0500

    starting section 12.13

---
+++ Section_12/lesson58.js
@@ -0,0 +1,27 @@
+$(function() {
+    // section 12.13
+    // Connect more web APIs
+
+    $('#output1, #output2').css({
+        'position': 'relative'
+    });
+
+    $('.btn').css({
+        'border': '1px solid black',
+        'width': '150px',
+        'padding': '10px'
+    });
+
+    $('.btn-1').click(function() {
+
+    })
+
+    $('.btn-2').click(function() {
+
+    })
+
+    $('.btn-3').click(function() {
+
+    })
+
+})
